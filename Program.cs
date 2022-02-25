7d8ad6b63c837716400dc6bc9adf61f9f3086c98
GOWDHAM19 <98729073+GOWDHAM19@users.noreply.github.com>
2022-02-25 13:07:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace DEVELOP_CSHARPINTERFACE
+{
+    interface AAPNA
+    {
+        void calculateArea(int a, int b);
+    }
+    class Rectangle : AAPNA
+    {
+        public void calculateArea(int a,int b)
+        {
+            int area = a * b;
+            Console.WriteLine("Area of Rectangle is :"+area);
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle gow = new Rectangle();
+            gow.calculateArea(10000,1234567890);
+        }
+    }
+}
